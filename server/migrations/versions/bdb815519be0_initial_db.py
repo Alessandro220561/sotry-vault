4d22079393cd826db75e6a0b028a066fd38677b0
Alessandro220561 <124537105+Alessandro220561@users.noreply.github.com>
2024-03-12 15:32:14 -0400

    initial commit

---
+++ server/migrations/versions/bdb815519be0_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: bdb815519be0
+Revises: 
+Create Date: 2024-03-12 15:32:03.258954
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bdb815519be0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
