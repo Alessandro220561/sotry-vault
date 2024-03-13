859624eb0e9b6c553bf2fc76a30fdd2e0cac8c82
Alessandro220561 <124537105+Alessandro220561@users.noreply.github.com>
2024-03-13 19:57:48 -0400

    login back end route initialization

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from config import app, api
+from config import app, api, db
 from flask import request, make_response, session, jsonify
 from flask_restful import Resource
 from models import User, Book, ReadingLog
@@ -39,6 +39,18 @@ class Signup(Resource):
             return ('Error in signing up', 422)
         
 api.add_resource(Signup, '/signup')
+
+class Login(Resource):
+    def post(self):
+        try:
+            user = User.query.filter_by(username=request.get_json()['username']).first()
+            if user.authenticate(request.get_json()['password']):
+                session['user_id'] = user.id
+                return make_response(user.to_dict(), 200)
+        except:
+            return ("Incorrect Username or Password", 401)
+        
+api.add_resource(Login, '/login')
 
 if __name__ == "__main__":
   app.run(port=5555, debug=True)
